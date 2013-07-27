091d3c71beb67ab116ecc4cfd70df46b1013eb96
alexancarlProject <alexancarlProject@d79c518b-7b79-1fd6-2dda-261e45ad219f>
2013-07-27 03:09:19 +0000

    Primera Subida

---
+++ Central/ScriptBd/09.dgs015_modifica.sql
@@ -0,0 +1,15 @@
+﻿-- --------------------------------------------------------------------------------
+-- Routine DDL
+-- Note: Modifica los datos de un formato DGS015
+-- --------------------------------------------------------------------------------
+DELIMITER $$
+
+CREATE PROCEDURE `sce_central_db`.`dgs015_modifica` (
+    in p_dgs015_id int,
+    in p_dgs_tipo_producto int
+)
+BEGIN
+    update dgs015
+       set dgs_tipo_producto = p_dgs_tipo_producto
+     where dgs015_id = p_dgs015_id;
+END
