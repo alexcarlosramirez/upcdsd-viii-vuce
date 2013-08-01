c5463655fd14c3c79975a7b7c777ee2b038bb65e
alexancarlproject <alexancarlproject@d79c518b-7b79-1fd6-2dda-261e45ad219f>
2013-08-01 18:08:46 +0000

    Versión funcional

--- Entidad/Entidad.WebServices/conexion/DBManager.php
+++ Entidad/Entidad.WebServices/conexion/DBManager.php
@@ -2,28 +2,50 @@
 class ConSQL
 {
 	private $conexion;
-	public function Conectar()
-	{
-		$this->conexion = mysql_connect("localhost:3306","root","root");
-		if(!$this->conexion)
-		{
-			echo "No se ha podido conectar a la base de datos.";
-		}
-		else
-		{
-			mysql_select_db("sd_entidad",$this->conexion);
-		}
-	} 
 
-	public function Consulta($sql)
+	public function connect()
 	{
-		$resultado = mysql_query($sql,$this->conexion); 
-		if(!$resultado)
+		$this->conexion = new mysqli("localhost:3306","root","root","sd_entidad");
+		if (mysqli_connect_errno())
 		{
-			echo "Error en la base de datos: ".mysql_error();
-			exit;
+			error_log("Entidad.WebServices DBManager->connect failed: %s\n");
+			exit();
 		}
-		return $resultado;
+	}
+
+	public function query($sql)
+	{
+		try
+		{
+			$result = $this->conexion->query($sql);
+			if ($result == null)
+			{
+				$error = mysqli_error($this->conexion);
+				error_log("Entidad.WebServices DBManager->query result failed:\n$sql\n$error");
+			}
+			return $result;
+		}
+		catch (Exception $e)
+		{
+			error_log("Entidad.WebServices DBManager->query  failed: \n".$e);
+			return null;
+		}
+	}
+
+	public function beginTransacction() {
+		$this->conexion->autocommit(FALSE);
+	}
+
+	public function commit() {
+		$this->conexion->commit();
+		$this->conexion->autocommit(TRUE);
+		$this->conexion->close();
+	}
+
+	public function rollback() {
+		$this->conexion->rollback();
+		$this->conexion->autocommit(TRUE);
+		$this->conexion->close();
 	}
 }
 ?>
