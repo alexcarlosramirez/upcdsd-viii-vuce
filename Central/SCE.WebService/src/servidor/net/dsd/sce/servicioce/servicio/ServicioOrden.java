3ae0420a0cd30e51cdf64556e8f4cd263dd5e064
alexancarlproject <alexancarlproject@d79c518b-7b79-1fd6-2dda-261e45ad219f>
2013-08-01 04:45:44 +0000

    Se elimina para regenerar

--- Central/SCE.WebService/src/servidor/net/dsd/sce/servicioce/servicio/ServicioOrden.java
+++ Central/SCE.WebService/src/servidor/net/dsd/sce/servicioce/servicio/ServicioOrden.java
@@ -1,14 +1,10 @@
 package net.dsd.sce.servicioce.servicio;
 
-import java.util.ArrayList;
-
+import net.dsd.sce.bean.BeanFormato;
 import net.dsd.sce.bean.BeanOrden;
+import net.dsd.sce.bean.BeanUsuario;
 import net.dsd.sce.bean.digesa.BeanDgs015;
-import net.dsd.sce.bean.digesa.BeanDgs015Producto;
 import net.dsd.sce.servicio.MysqlOrdenDao;
-import net.dsd.sce.transmitirdigesarequest.Dgs015ProductoType;
-import net.dsd.sce.transmitirdigesarequest.Dgs015Type;
-import net.dsd.sce.transmitirdigesarequest.Dgs016Type;
 
 public class ServicioOrden {
 	private MysqlOrdenDao mysqlOrdenDao;
@@ -25,29 +21,25 @@ public class ServicioOrden {
 		return mysqlOrdenDao.buscarOrdenPorCda(cda);
 	}
 
-	public void actualizarDgs015(BeanOrden orden, Dgs015Type dgs015Type) {
-		Dgs015ProductoType[] listaDgs015ProductoType = dgs015Type.getListaProducto();
-		BeanDgs015 dgs015 = new BeanDgs015();
-		dgs015.setDgs015Id(orden.getFormatoEntidadId());
-		dgs015.setDgsTipoProducto(dgs015Type.getTipoProducto());
-		ArrayList<BeanDgs015Producto> listaDgs015Producto = new ArrayList<BeanDgs015Producto>();
-		for (Dgs015ProductoType dgs015ProductoType : listaDgs015ProductoType) {
-			BeanDgs015Producto dgs015Producto = new BeanDgs015Producto();
-			dgs015Producto.setDgs015Id(orden.getFormatoEntidadId());
-			dgs015Producto.setNombre(dgs015ProductoType.getNombre());
-			dgs015Producto.setPartidaArancelaria(dgs015ProductoType.getPartidaArancelaria());
-			dgs015Producto.setCantidad(dgs015ProductoType.getCantidad());
-			dgs015Producto.setEnvase(dgs015ProductoType.getEnvase());
-			listaDgs015Producto.add(dgs015Producto);
-		}
-
-		mysqlOrdenDao.registrarDgs015(dgs015, listaDgs015Producto);
+	public BeanOrden buscarOrdenPorOrdenId(int ordenId) {
+		return mysqlOrdenDao.buscarOrdenPorOrdenId(ordenId);
 	}
 
-	public void actualizarDgs016(BeanOrden orden, Dgs016Type formatoDgs016) {
-		// TODO Auto-generated method stub
-		
+	public BeanFormato buscarFormatoEntidadPorOrdenId(int ordenId) {
+		return null;
 	}
 
-	
+	public BeanUsuario buscarUsuarioSolicitantePorOrdenId(int ordenId) {
+		return mysqlOrdenDao.buscarUsuarioSolicitantePorOrdenId(ordenId);
+	}
+
+	//Digesa
+
+	public BeanDgs015 buscarDgs015PorOrdenId(int ordenId) {
+		return mysqlOrdenDao.buscarDgs015PorOrdenId(ordenId);
+	}
+
+	public void actualizarDgs015(BeanOrden orden, BeanDgs015 dgs015) {
+		mysqlOrdenDao.registrarDgs015(dgs015, dgs015.getListaDgs015Producto());
+	}
 }
